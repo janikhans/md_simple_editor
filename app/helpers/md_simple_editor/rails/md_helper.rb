eddef5d364bc851f66c4271ce387c6ba57e5070e
Janik Knittle <janik.knittle@gmail.com>
2018-06-26 18:11:38 -0700

    Refactor helper

--- app/helpers/md_simple_editor/rails/md_helper.rb
+++ app/helpers/md_simple_editor/rails/md_helper.rb
@@ -8,88 +8,70 @@ module MdSimpleEditor
       def md_simple_editor(klass = '')
         @md_builder = MdBuilder.new(klass)
         content_tag(:div, id: 'md-editor') do
-          content_tag(:div, class: 'btn-toolbar', role: 'toolbar') do
-            header_tags + text_tools + link_and_image_tools +
-              content_tag(:div, class: 'btn-group pull-right') do
-                button_tag(type: 'button', class: 'btn btn-primary preview_md') do
-                  'Preview'
-                end
-              end
-          end +
-            content_tag(:br) +
-            content_tag(:div, id: 'md-text') do
-              yield
-            end +
-            content_tag(:div, class: 'panel panel-success preview-panel', hidden: true) do
-              content_tag(:div, class: 'panel-heading') do
-                'Preview'
-              end +
-                content_tag(:div, id: 'md-preview', class: 'panel-body') do
-                end
-            end
+          concat(toolbar)
+          concat(tag(:br))
+          concat(content_tag(:div, id: 'md-text') { yield })
+          concat(preview_panel)
         end
       end
 
+      private
+
       def header_tags
+        headers = %w[H1 H2 H3 H4 H5]
         content_tag(:div, class: 'btn-group') do
-          button_tag(type: 'button', class: "#{@md_builder.default_class} md_h1") do
-            content_tag(:strong, 'H1')
-          end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_h2") do
-              content_tag(:strong, 'H2')
-            end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_h3") do
-              content_tag(:strong, 'H3')
-            end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_h4") do
-              content_tag(:strong, 'H4')
-            end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_h5") do
-              content_tag(:strong, 'H5')
-            end
+          headers.each do |header|
+            concat(header_button(header))
+          end
         end
       end
 
       def link_and_image_tools
+        tools = ['link', 'camera-retro']
         content_tag(:div, class: 'btn-group') do
-          button_tag(type: 'button', class: "#{@md_builder.default_class} md_link") do
-            fa_icon 'link'
-          end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_camera-retro") do
-              fa_icon 'camera-retro'
-            end
+          tools.each do |tool|
+            concat(tool_button(tool))
+          end
         end
       end
 
       def text_tools
+        tools = [
+          'italic', 'bold', 'list-ul', 'list-ol', 'indent',
+          'underline', 'table', 'square', 'minus'
+        ]
         content_tag(:div, class: 'btn-group') do
-          button_tag(type: 'button', class: "#{@md_builder.default_class} md_italic") do
-            fa_icon 'italic'
-          end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_bold") do
-              fa_icon 'bold'
-            end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_list-ul") do
-              fa_icon 'list-ul'
-            end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_list-ol") do
-              fa_icon 'list-ol'
-            end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_indent") do
-              fa_icon 'indent'
-            end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_underline") do
-              fa_icon 'underline'
-            end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_table") do
-              fa_icon 'table'
-            end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_square") do
-              fa_icon 'square'
-            end +
-            button_tag(type: 'button', class: "#{@md_builder.default_class} md_minus") do
-              fa_icon 'minus'
-            end
+          tools.each do |tool|
+            concat(tool_button(tool))
+          end
+        end
+      end
+
+      def tool_button(tool)
+        button_tag(type: 'button', class: "#{@md_builder.default_class} md_#{tool}") do
+          fa_icon(tool)
+        end
+      end
+
+      def header_button(header)
+        button_tag(type: 'button', class: "#{@md_builder.default_class} md_#{header.downcase}") do
+          content_tag(:strong, header)
+        end
+      end
+
+      def toolbar
+        content_tag(:div, class: 'btn-toolbar', role: 'toolbar') do
+          concat(header_tags)
+          concat(text_tools)
+          concat(link_and_image_tools)
+          concat(button_tag('Preview', type: 'button', class: 'btn btn-primary preview_md'))
+        end
+      end
+
+      def preview_panel
+        content_tag(:div, class: 'preview-panel', hidden: true) do
+          concat(content_tag(:div, 'Preview', class: 'alert alert-info'))
+          concat(content_tag(:div, nil, id: 'md-preview', class: 'panel-body'))
         end
       end
     end
